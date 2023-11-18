6fb017dcadeb156ca3e6761f6954f9e0ec8b58d2
Arjun Tanwar <arjunthakur900@gmail.com>
2023-11-18 17:21:06 +0530

    working setup

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
+import figlet from 'figlet';
+console.log(Bun.version);
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-      return new Response("Bun!");
+      const body = figlet.textSync("Bun!");
+      return new Response(body);
     },
   });
   
